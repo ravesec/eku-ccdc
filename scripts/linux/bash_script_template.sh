bc1e6d465c4110735857d0d39f48af6cc85bb8d7
Raven <ravenn@tuta.io>
2024-02-04 04:31:53 -0500

    Formatting changes.

--- scripts/linux/bash_script_template.sh
+++ scripts/linux/bash_script_template.sh
@@ -3,25 +3,26 @@
 # Author: <author(s)>
 # script_name.sh
 #
-# Description:
-# 
+# Description: A template for bash scripts created by Raven Dean. NOTE that the echo command is overriden to identify the script
+#  that is putting output on the terminal.
 #
 # Dependencies: N/A
 # Created: MM/DD/YYYY
 # Usage: <./script_name.sh <args>>
 
-usage="\e[31m./script_name.sh <args>\e[0m"
+# Edit these as required. ANSI color coding is used.
+script_name="script_name.sh"
+usage="\e[31m./$script_name <args>\e[0m"
+
+# Import environment variables
+. ../../config_files/ekurc
 
 if [ "$EUID" -ne 0 ] # Superuser requirement. Echo the error to stderr and return exit code 1.
-then echo -e "\e[31mERROR: This script must be ran as root!\e[0m" >&2
+then echo "\e[31mError: This script must be ran as root!\e[0m" >&2
     exit 1
 fi
 
-# Import environment variables
-. ../../config_files/env
-
 # Main script here...
-
 
 exit 0 # Script ended successfully
 

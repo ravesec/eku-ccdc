1d195dfb4a1967216e22a091f8d821d64bb8667f
Raven <ravenn@tuta.io>
2024-02-07 12:35:38 -0500

    fixed several typos

--- scripts/ansible-playbooks/setup.sh
+++ scripts/ansible-playbooks/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+script_name="setup.sh"
+source ../../config_files/ekurc
 sudo apt update
 sudo apt install --yes apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -7,16 +9,5 @@ apt-cache policy docker-ce
 sudo apt install --yes docker-ce
 sudo systemctl enable docker
 sudo docker build --no-cache -t ekuccdc:latest docker/
-sudo docker run --rm --name ekuccdc --volume /home/sysadmin:/root/shared --hostname ekuccdc-docker ekuccdc:latest /bin/bash -c "cp /root/.ssh/id_rsa.pub /root/shared/id_rsa.pub && sleep 45 && ansible-playbook -i inventory configure.yml"
-
-while true; do
-  sudo docker inspect ekuccdc > /dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    success "Firewall configured successfully!"
-    break
-  else
-    warn "Firewall configuration script running..."
-    sleep 5
-  fi
-done
-
+sudo docker run --rm -it --name ekuccdc --hostname ekuccdc-docker ekuccdc:latest /bin/bash -c "nano configure.yml && ansible-playbook -i inventory configure.yml"
+success "Script complete!"

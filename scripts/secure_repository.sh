41e0048bc648f7e3ae7f8b79247b8afcbdafcc89
Raven <ravenn@tuta.io>
2024-02-07 15:41:59 -0500

    change repository permissions

---
+++ scripts/secure_repository.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+script_name="script_name.sh"
+usage="./$script_name"
+
+# Import environment variables
+. ../../config_files/ekurc
+
+if [ "$EUID" -ne 0 ] # Superuser requirement.
+then error "This script must be ran as root!"
+    exit 1
+fi
+
+# Check for the correct number of arguments
+if [ "$#" -gt 0 ]
+then error $usage
+    exit 1
+fi
+
+# Find the repo root and do o-rwx, g-w
+repo_dir="$(find / -type d -name 'eku-ccdc' 2>/dev/null)"
+find $repo_dir -exec chmod 750 -- {} +
+success "Fixed repo permissions!"
+
+exit 0 # Script ended successfully
+

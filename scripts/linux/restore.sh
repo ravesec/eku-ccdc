050fc667efdf1b957e270bd8492af9aeac8ef897
Raven <ravenn@tuta.io>
2024-02-07 00:55:36 -0500

    file exists check

---
+++ scripts/linux/restore.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Author: Raven Dean
+# restore.sh
+#
+# Description: This script restores files that were backed up using ./backup.sh. It includes checks
+#  to ensure nothing was tampered with. 
+#
+# Dependencies: N/A
+# Created: 02/04/2024
+# Usage: ./restore.sh
+
+script_name="restore.sh"
+usage="./restore.sh <directory>"
+
+#TODO: Provide the user with a menu of all current backups.
+
+# Import environment variables
+. ../../config_files/ekurc
+
+if [ "$EUID" -ne 0 ] # Superuser requirement. Echo the error to stderr and return exit code 1.
+then error "This script must be ran as root!"
+    exit 1
+fi
+
+# Check for the correct number of arguments
+if [ "$#" -ne 1 ]
+then error $usage
+    exit 1
+fi
+
+# Check if the supplied directory exists in the backups folder
+backup_name=$(realpath $1)
+if [[ ! -f "$backup_dir/$backup_name.tar.gz" ]]
+then
+    error "'$1' does not exist in the backups folder!"
+    exit 1
+fi
+
+
+
+# Check if the backup and it's checksum are still immutable
+
+# Validate the backup's checksum
+
+# Everything seems okay... restore the directory from the backup.
+
+
+exit 0 # Script ended successfully
+

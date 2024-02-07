d6d71c258c2987e6dd0d8eba3b5fc38f9448c992
Raven <ravenn@tuta.io>
2024-02-06 19:29:34 -0500

    hard coded pass :)

--- scripts/linux/prestashop.sh
+++ scripts/linux/prestashop.sh
@@ -13,25 +13,24 @@
 script_name="prestashop.sh"
 usage="./$script_name"
 
+sql_pass="changeme"
+
 # Import environment variables
 . ../../config_files/ekurc
-
 if [ "$EUID" -ne 0 ] # Superuser requirement. Echo the error to stderr and return exit code 1.
 then error "This script must be ran as root!"
     exit 1
 fi
 
 # Check for the correct number of arguments
-if [ "$EUID" -gt 1 ]
+if [ "$EUID" -gt 0 ]
 then error $usage
     exit 1
 fi
 
 # Set the mysql root password
-new_pass=$(ask_password "the mysql root user")
-
-mysql -u root -p<<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$new_pass';
+mysql -u root<<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$sql_pass';
 FLUSH PRIVILEGES;
 EOF
 
@@ -42,10 +41,9 @@ mysql_secure_installation
 # The password of prestashop users is salted with the cookie key. Save the cookie key to a variable and change the password.
 key_path="/var/www/html/prestashop/config/settings.inc.php"
 key="$(cat $key_path | grep "_COOKIE_KEY_" | cut -d ',' -f2 | tr -d " ,');")"
-query="UPDATE ps_employee SET passwd = MD5('$key$(ask_password "the prestashop admin user")') WHERE firstname = 'Greg';"
+query="UPDATE ps_employee SET passwd = MD5('$key$(ask_password)') WHERE firstname = 'Greg';"
 
-mysql -u"root" -p"$new_pass" -D"prestashop" -e"$query" --verbose
-unset new_pass
+mysql -u"root" -p"$sql_pass" -D"prestashop" -e"$query" --verbose
 
 exit 0 # Script ended successfully
 

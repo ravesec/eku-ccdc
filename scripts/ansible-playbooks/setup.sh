16d79a83fc3cc55d0f2b5aca92eab66649619a4d
Raven <ravenn@tuta.io>
2024-02-07 10:54:42 -0500

    ubuntu pre fw-setup

---
+++ scripts/ansible-playbooks/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+. ../../config_files/ekurc
+sudo apt update
+sudo apt install --yes apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo start-ansible

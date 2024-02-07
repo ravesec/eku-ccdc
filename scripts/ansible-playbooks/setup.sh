a072a34c5f715cc20c1c6bab2d5ca1aa63a45fbf
Raven <ravenn@tuta.io>
2024-02-07 11:18:37 -0500

    fixed several typos

--- scripts/ansible-playbooks/setup.sh
+++ scripts/ansible-playbooks/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-. ../../config_files/ekurc
 sudo apt update
 sudo apt install --yes apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install --yes docker-ce
 sudo systemctl enable docker
 sudo systemctl status docker
-sudo start-ansible
+sudo docker build --no-cache -t ekuccdc:latest docker/
+sudo docker run --rm --it --name ekuccdc --volume /home/sysadmin:/root/shared --hostname ekuccdc-docker ekuccdc:latest

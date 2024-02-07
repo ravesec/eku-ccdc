a4b52d3ea2f65b3b59293d5bf524734635cd9c57
Raven <ravenn@tuta.io>
2024-02-07 12:19:36 -0500

    fixed several typos

--- scripts/ansible-playbooks/setup.sh
+++ scripts/ansible-playbooks/setup.sh
@@ -7,4 +7,16 @@ apt-cache policy docker-ce
 sudo apt install --yes docker-ce
 sudo systemctl enable docker
 sudo docker build --no-cache -t ekuccdc:latest docker/
-sudo docker run --rm -it --name ekuccdc --volume /home/sysadmin:/root/shared --hostname ekuccdc-docker ekuccdc:latest
+sudo docker run --rm --name ekuccdc --volume /home/sysadmin:/root/shared --hostname ekuccdc-docker ekuccdc:latest /bin/bash -c "cp /root/.ssh/id_rsa.pub /root/shared/id_rsa.pub && sleep 45 && ansible-playbook -i inventory configure.yml"
+
+while true; do
+  sudo docker inspect ekuccdc > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    success "Firewall configured successfully!"
+    break
+  else
+    warn "Firewall configuration script running..."
+    sleep 5
+  fi
+done
+

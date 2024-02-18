cc39976cabf984a25c42a73399b18ecd884ac645
Raven <ravenn@tuta.io>
2024-02-17 22:44:56 -0500

    final commit before freeze

--- scripts/linux/secure_repository.sh
+++ scripts/linux/secure_repository.sh
@@ -4,7 +4,10 @@ script_name="secure_repository.sh"
 usage="./$script_name"
 
 # Import environment variables
-source ../../config_files/ekurc
+
+# Find the repo root
+repo_dir="$(find / -type d -name 'eku-ccdc' 2>/dev/null)"
+source "$repo_dir/config_files/ekurc"
 
 if [ "$EUID" -ne 0 ] # Superuser requirement.
 then error "This script must be ran as root!"
@@ -17,11 +20,9 @@ then error $usage
     exit 1
 fi
 
-# Find the repo root and do o-rwx, g-w
-repo_dir="$(find / -type d -name 'eku-ccdc' 2>/dev/null)"
 find $repo_dir -exec chmod 0750 -- {} +
 chattr +a "$repo_dir/config_files/.history"
-chattr +i "$repo_dir/scripts/linux/slack/api_key" "$repo_dir/scripts/linux/slack/webhook_url"
+chattr +i "$repo_dir/scripts/linux/slack/api_key" "$repo_dir/scripts/linux/slack/webhook_link"
 success "Fixed repo permissions!"
 
 exit 0 # Script ended successfully

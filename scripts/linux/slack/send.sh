74555a5d971b9212006b848bd4879d4ba5a30d2a
Raven <ravenn@tuta.io>
2024-02-17 21:36:44 -0500

    send script

---
+++ scripts/linux/slack/send.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Author: Raven Dean
+# send.sh
+#
+# Description: Sends strings to a slack channel using webhooks. Check the README for instructions
+#
+# Dependencies: ./webhook_link curl
+# Created: 02/17/2024
+# Usage: <./send.sh <message_to_send>>
+
+aes_password="ekuccdc2024!"
+
+script_name="send.sh"
+usage="./$script_name <message>"
+
+# Import environment variables
+source ../../../config_files/ekurc
+
+# Check for the correct number of arguments
+if [ "$#" -lt 1 ]
+then error $usage
+    exit 1
+fi
+
+# Check repository security requirement
+check_security
+
+if [ -z "$aes_password" ]
+then
+    error "Password not set! Edit this script to change the password."
+    exit 1
+fi
+
+info "This script requires 'openssl' and 'jq'!"
+webhook_url=$(openssl enc -aes-256-cbc -d -salt -pass pass:$aes_password -in ./webhook_link 2>/dev/null)
+
+if [ "$?" -eq 1 ]
+then
+    error "Decryption of webhook url failed. Is the password set correctly?"
+    exit 1
+fi
+
+message="$@"
+payload=$(jq -nc --arg text "$message" '{"text": $text}')
+
+# Send the message.
+curl --silent -X POST -H 'Content-type: application/json' --data "$payload" "$webhook_url" 2>&1 > /dev/null
+
+if [ "$?" -ne 0 ]
+then
+    error "Message failed to send!"
+    exit 1
+fi
+
+exit 0 # Script ended successfully
+

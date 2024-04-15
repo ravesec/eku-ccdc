53f36deef9a1e2ef1cd92f604e7c643227eedaa4
Sam Burton <sam.burton271@gmail.com>
2024-04-15 15:12:31 -0400

    Added fixed setup code(-h for help), fixed some naming issues, still trying to fix failure to execute script remotely

--- scripts/splunk/SecurityAutomation/setup.py
+++ scripts/splunk/SecurityAutomation/setup.py
@@ -6,26 +6,57 @@ import time
 
 ips = ["172.20.240.20", "172.20.241.30", "172.20.241.40", "172.20.242.10"] #TODO: Change these for your network, these are default for ccdc environemnts as of 2023-2024
 def main():
-    os.system("curl -o /etc/setup https://github.com/ravesec/eku-ccdc/scripts/splunk/SecurityAutomation/setup.sh")
-    for ip in ips:
-        name = switch(ip)
-        sendFile(ip, "/etc/setup", "/etc/setup", name)
-        os.system("stty -echo")
-        password = input(f"Enter password for {name}\'s root(\'b\' to bypass: " )
-        os.system("stty echo")
-        if(input.lower() in ("b")):
-            print(f"Bypassing {name}")
+    if(len(sys.argv) != 1):
+        if(sys.argv[1] == "-t" or sys.argv[1] == "-T"):
+            os.system("mv /etc/eku-ccdc/scripts/splunk/SecurityAutomation/setup.sh /etc/setup")
+            x = True
+            while(x):
+                ip = input("Enter IP Address: ")
+                print("")
+                if(switch(ip) == "NOT FOUND"):
+                    print("Unknown IP. Try again.")
+                else:
+                    x = False
+            name = switch(ip)
+            sendFile(ip, "/etc/setup", "/etc/destSetup", name)
+            os.system("stty -echo")
+            password = input(f"Enter password for {name}\'s root: " )
+            os.system("stty echo")
+            print("")
+            else:
+                ssh_client = paramiko.SSHClient()
+                ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                ssh_client.connect(ip, username="root", password=password)
+                execute("/etc/setup", ssh_client, name)
         else:
-            ssh_client = paramiko.SSHClient()
-            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh_client.connect(ip, username="root", password=password)
-            execute("/etc/setup", ssh_client, name)
+            print("""
+-h    |    Help menu (This guy)
+
+-t    |    Targeted setup. Asks for specific ip address. Designed to be used for reinstalls or in the case of a machine reset.
+
+""")
+    else:
+        os.system("mv /etc/eku-ccdc/scripts/splunk/SecurityAutomation/setup.sh /etc/setup")
+        for ip in ips:
+            name = switch(ip)
+            sendFile(ip, "/etc/setup", "/etc/destSetup", name)
+            os.system("stty -echo")
+            password = input(f"Enter password for {name}\'s root(\'b\' to bypass): " )
+            os.system("stty echo")
+            print("")
+            if(password.lower() in ("b")):
+                print(f"Bypassing {name}")
+            else:
+                ssh_client = paramiko.SSHClient()
+                ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                ssh_client.connect(ip, username="root", password=password)
+                execute("/etc/setup", ssh_client, name)
 def sendFile(address, source, dest, nameAddress):
     scp_command = f'scp {source} root@{address}:{dest}'
     subprocess.run(scp_command, shell=True, check=True)
     print(f"Script copied to {nameAddress}")
 def execute(path, ssh_client, name):
-    stdin, stdout, stderr = ssh_client.exec_command(f"sudo python3 {path}")
+    stdin, stdout, stderr = ssh_client.exec_command(f"bash {path}")
     print(f"Executing setup script on {name}...")
     time.sleep(5)
 def switch(ip):
@@ -37,4 +68,6 @@ def switch(ip):
         return "Fedora"
     elif ip == "172.20.242.10":
         return "Ubuntu"
+    else:
+        return "NOT FOUND"
 main()

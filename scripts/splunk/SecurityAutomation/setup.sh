3f0b03746853b0a2b9226ba4680cc1994a0d7687
Sam Burton <sam.burton271@gmail.com>
2024-04-16 10:07:08 -0400

    Adding support for CentOS and the foundation of the listening service.

--- scripts/splunk/SecurityAutomation/setup.sh
+++ scripts/splunk/SecurityAutomation/setup.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
-wall "test"
+machine=$1
+if [ $machine=="centos" ]
+then
+cat <<EOFA > /etc/systemd/system/security.service
+[Unit]
+Description=Service used for splunk alert automation for EKU's CCDC team.
+
+[Service]
+Type=forking
+Environment="PATH=/sbin:/bin:/usr/sbin:/usr/bin"
+ExecStart=python3 /etc/secService.py centos
+StartLimitInterval=1s
+StartLimitBurst=999
+
+[Install]
+WantedBy=multi-user.target
+EOFA
+yum install -y python3
+
+fi
+if [ $machine=="debian" ]
+then
+apt-get update && apt-get install -y python3
+echo "kdsajf;ds"
+fi
+if [ $machine=="fedora" ]
+then
+yum install -y python3
+echo "dwehqfjek"
+fi
+if [ $machine=="ubuntu" ]
+then
+apt-get update && apt-get install -y python3
+echo "dswqgudhq"
+fi

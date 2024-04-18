b55fd679ee7879b8533c9eedf611ade488f1cae2
Sam Burton <sam.burton271@gmail.com>
2024-04-18 11:28:00 -0400

    Added local utility script to bin

--- scripts/splunk/SecurityAutomation/secService.py
+++ scripts/splunk/SecurityAutomation/secService.py
@@ -1,3 +1,5 @@
+#CHANGES REQUIRE RESTART OF SERVICE THROUGH "SECURITY" COMMAND TO TAKE EFFECT
+
 import paramiko
 import os
 import sys
@@ -8,7 +10,7 @@ def main():
     machine = sys.argv[1]
     while(True):
         user = machine+"User"
-        password = "changeme"
+        password = "changeme" #TODO: Change this password to whatever the user password is
         ip = "172.20.241.20"
         
         ssh_client = paramiko.SSHClient()
@@ -20,9 +22,18 @@ def main():
         error = stderr.read().decode('utf-8')
         if(len(error) > 1):
             os.system(f"wall \"An error occured with secService: \" {error}")
-        
+        response(output)
         time.sleep(30)
 def response(value):
     if(value == "999"):
         os.system("wall \"Splunk has detected a possible compromise of itself. Stopping all secService instances on all machines. Manual service restart required.\"")
+        os.system("systemctl disable security.service")
+        killProcess(secService.py)
+def killProcess(name):
+    ps_output = subprocess.check_output(["ps", "-ef"])
+    ps_lines = ps_output.decode("utf-8").split("\n")
+    for line in ps_lines:
+        if name in line:
+            pid = int(line.split(None, 1)[1].split()[0])
+            os.kill(pid, 9)
 main()

4dc92f30b71acc9ad189b6a36821c1e02277cca6
Sam Burton <sam.burton271@gmail.com>
2024-04-22 14:21:46 -0400

    Added new form of alert automation.

--- scripts/splunk/SecurityAutomation/setup.sh
+++ scripts/splunk/SecurityAutomation/setup.sh
@@ -2,30 +2,9 @@
 machine=$1
 if [ $machine=="centos" ]
 then
-cat <<EOFA > /etc/systemd/system/security.service
-[Unit]
-Description=Service used for splunk alert automation for EKU's CCDC team.
-
-[Service]
-Type=forking
-Environment="PATH=/sbin:/bin:/usr/sbin:/usr/bin"
-ExecStart=python3 /etc/secService.py centos
-StartLimitInterval=1s
-StartLimitBurst=999
-
-[Install]
-WantedBy=multi-user.target
-EOFA
 yum install -y python3
-mv /etc/eku-ccdc/scripts/splunk/SecurityAutomation/secService.py /etc/secService.py
-mv /etc/eku-ccdc/scripts/splunk/SecurityAutomation/security.sh /usr/bin/security
-mv /etc/eku-ccdc/scripts/splunk/SecurityAutomation/security.py /usr/bin/security.py
-chmod +x /usr/bin/security
-chmod +x /usr/bin/security.py
-rm /etc/eku-ccdc/scripts/splunk/SecurityAutomation/secBrain.py
-systemctl daemon reload
-systemctl enable security.service
-systemctl start security.service
+adduser splunkListener
+echo splunkListener:changeme | chpasswd
 fi
 if [ $machine=="debian" ]
 then

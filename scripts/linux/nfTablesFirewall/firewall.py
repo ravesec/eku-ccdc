0fea6415b2fd70e4f8dc57b44ce00753dc5f185c
Sam Burton <sam.burton271@gmail.com>
2024-05-14 12:10:20 -0400

    Added root-only restriction. Adding commands for tables/chains within nftables ip family

--- scripts/linux/nfTablesFirewall/firewall.py
+++ scripts/linux/nfTablesFirewall/firewall.py
@@ -1,11 +1,15 @@
 import os
 import subprocess
 import sys
+from datetime import datetime
 
 def main():
-    if(len(sys.argv[] == 2)):
-        if(sys.argv[1].lower() in ('-h', '--help', 'help')):
-            print("""
+    if(os.getuid() != 0):
+        print("Access Denied. Must be run as root.")
+    else:
+        if(len(sys.argv[] == 2)):
+            if(sys.argv[1].lower() in ('-h', '--help', 'help')):
+                print("""
 Firewall interface for linux machines using nftables. Written for use by EKU's CCDC team in practice and live environments.
 
 
@@ -18,30 +22,30 @@ In-Program Commands:
 
 help           |     Displays this help menu.
 """)
-    else:
-        tableList = getTableList()
-        x = True
-        while(x):
-            option = input("[Command@Core]# ")
-            if(option.lower() in ('table')):
-                print("List of tables: ")
-                for table in tableList:
-                    print(table)
-                option = input("Which table would you like to move to? ")
-                y = False
-                while(!y):
+        else:
+            tableList = getTableList()
+            x = True
+            while(x):
+                option = input("[Command@Core]# ")
+                if(option.lower() in ('table')):
+                    print("List of tables: ")
                     for table in tableList:
-                        if(table == option):
-                            y = True
-                    if(!y):
-                        print("Invalid selection.")
-                tableCommand(option)
-            elif(option.lower() in ('add')):
-                print("Adding a table...")
-                name = input("Enter table name: ")
-                os.system("nft add table "+name)
-            else:
-                print("""
+                        print(table)
+                    option = input("Which table would you like to move to? ")
+                    y = False
+                    while(!y):
+                        for table in tableList:
+                            if(table == option):
+                                y = True
+                        if(!y):
+                            print("Invalid selection.")
+                    tableCommand(option)
+                elif(option.lower() in ('add')):
+                    print("Adding a table...")
+                    name = input("Enter table name: ")
+                    os.system("nft add table "+name)
+                else:
+                    print("""
 Firewall interface for linux machines using nftables. Written for use by EKU's CCDC team in practice and live environments.
 
 
@@ -56,10 +60,13 @@ In-Program Commands:
 
     Core Commands:
     
-    switch         |     Changes command focus to a specific table.
+    table          |     Changes command focus to a specific table.
     add            |     Adds a new table to the ip family.
     
     Table Commands:
+    
+    add            |     Adds a new chain to the selected table.
+    chain          |     Changes command focus to a specific chain within the table.
     
     Chain Commands:
 
@@ -68,11 +75,12 @@ In-Program Commands:
 def tableCommand(table):
     x = True
     while(x):
-        option = input(f"[Command@{table}# ")
+        option = input(f"[Command@{table}]# ")
         if(option.lower() in ('exit')):
             return
         elif(option.lower() in ('add')):
             print(f"Adding chain to {table}")
+        elif(option.lower() in ('chain')):
             
 def chainCommand(table, chain):
     x = True

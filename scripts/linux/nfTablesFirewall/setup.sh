b90319c724da79495206c9f6a866b68f94d17863
Sam Burton <sam.burton271@gmail.com>
2024-05-16 21:53:14 -0400

    Added rudementary setup script and command call with "fireall"

---
+++ scripts/linux/nfTablesFirewall/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mv /etc/eku-ccdc/scripts/linux/nfTablesFirewall/firewall.py /etc/firewall.py
+mv /etc/eku-ccdc/scripts/linux/nfTablesFirewall/firewall.sh /bin/firewall
+chmod +x /bin/firewall

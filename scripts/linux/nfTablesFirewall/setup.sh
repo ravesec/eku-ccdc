fb7f09069b16a0665f3d9ab481f219eedc279fc6
Sam Burton <sam.burton271@gmail.com>
2024-05-20 13:30:27 -0400

    Added functionality to setup script, will now check dependencies and enable firewall to detect remote connections through sudo

--- scripts/linux/nfTablesFirewall/setup.sh
+++ scripts/linux/nfTablesFirewall/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+apt-get update && apt-get install -y nftables &
+apt-get update && apt-get install -y python3 &
+yum install -y nftables &
+yum install -y python3 &
 mv /etc/eku-ccdc/scripts/linux/nfTablesFirewall/firewall.py /etc/firewall.py
 mv /etc/eku-ccdc/scripts/linux/nfTablesFirewall/firewall.sh /bin/firewall
 chmod +x /bin/firewall
+echo "Defaults env_keep += \"SSH_CONNECTION SSH_CLIENT SSH_TTY\"" >> /etc/sudoers

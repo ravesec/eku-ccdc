e8a0e2ba665f1ee5512349b46bfca2f4344a3b29
Sam Burton <sam.burton271@gmail.com>
2024-07-31 09:20:10 -0400

    Moved order of setup so firewall rules do not block ssh while trying to install socket listeners

--- scripts/linux/Manticore/setup.sh
+++ scripts/linux/Manticore/setup.sh
@@ -23,11 +23,6 @@ echo "Moving Manticore..."
 mkdir /etc/manticore
 mv /etc/eku-ccdc/scripts/linux/Manticore/* /etc/manticore
 rm /etc/manticore/setup.sh
-echo "Setting up firewall..."
-mv /etc/eku-ccdc/scripts/linux/nfTablesFirewall/firewall.py /bin/firewall
-chmod +x /bin/firewall
-echo "Defaults env_keep += \"SSH_CONNECTION SSH_CLIENT SSH_TTY\"" >> /etc/sudoers
-python3 /etc/eku-ccdc/scripts/linux/nfTablesFirewall/setup.py "splunk"
 echo "Preventing password changes..."
 chattr +i /etc/passwd
 chattr +i /opt/splunk/etc/passwd
@@ -37,3 +32,8 @@ echo "Setting up Fedora listener..."
 python3 /etc/manticore/netListenerSetup.py "172.20.241.40"
 #echo "Setting up Debian listener..."
 #echo "Setting up Ubuntu listener..."
+echo "Setting up firewall..."
+mv /etc/eku-ccdc/scripts/linux/nfTablesFirewall/firewall.py /bin/firewall
+chmod +x /bin/firewall
+echo "Defaults env_keep += \"SSH_CONNECTION SSH_CLIENT SSH_TTY\"" >> /etc/sudoers
+python3 /etc/eku-ccdc/scripts/linux/nfTablesFirewall/setup.py "splunk"

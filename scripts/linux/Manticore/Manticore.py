c4fe99b5b8906e988de07627e220a7c1415236fa
Sam Burton <sam.burton271@gmail.com>
2024-08-19 10:32:55 -0400

    Added function to undo the pseudo-encryption used for communications

--- scripts/linux/Manticore/Manticore.py
+++ scripts/linux/Manticore/Manticore.py
@@ -7,7 +7,13 @@ def main():
     for arg in sys.argv:
         arguments.append(arg)
     del(arguments[0])
-    print(encrypt("C17", "10.10.10.10"))
+    message = encrypt("C17", "10.10.10.10")
+    binary = message.encode('utf-8')
+    decoded = decrypt(message)
+    print(binary)
+    print(message)
+    print(decoded)
+    option = input("Ddd")
    
 def encrypt(code, address):
     message = []
@@ -33,4 +39,32 @@ def encrypt(code, address):
     message.append(addrCode)
     y = '-'
     return y.join(message)
+def decrypt(message):
+    decoded = []
+    messArray = message.split('-')
+    messArray[0] = int(messArray[0],0)
+    messArray[1] = int(messArray[1],0)
+    messArray[2] = int(messArray[2],0)
+    key = str(messArray[0])
+    keyOne = key[:1]
+    keyTwo = key[1:]
+    decoded.append(keyOne+keyTwo)
+    operator = int(keyOne) * int(keyTwo)
+    codeLet = messArray[1]
+    codeNum = messArray[2]
+    codeLet = chr(codeLet-operator)
+    codeNum = codeNum - operator
+    code = str(codeLet) + str(codeNum)
+    decoded.append(code)
+    address = messArray[3]
+    addrArray = address.split("?")
+    x = 0
+    for num in addrArray:
+        addrArray[x] = str(int(addrArray[x],0) - operator)
+        x = x + 1
+    y = '.'
+    address = y.join(addrArray)
+    decoded.append(address)
+    y = '-'
+    return y.join(decoded)
 main ()

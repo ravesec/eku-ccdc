95c8c2cf4cb28248b66b245c95cc8aa12f42a102
Sam Burton <sam.burton271@gmail.com>
2024-08-22 16:13:42 -0400

    Fixed color printing issues

--- scripts/linux/nfTablesFirewall/firewall.py
+++ scripts/linux/nfTablesFirewall/firewall.py
@@ -114,19 +114,19 @@ def main():
                         otherTablePres = True
                 print("EKU CCDC System Firewall Manager")
                 if(firewallPres):
-                    os.system("echo "+"Firewall Status: "+"\033[032m[GREEN]\033[0m")
+                    print("Firewall Status: \033[32;1m[GREEN]\033[0m")
                     if(not firewallInteg):
-                        os.system("echo "+"\033[133m[Caution: Firewall is active, however is missing a chain. Address this issue immediately.]\033[0m")
+                        print("\033[33;1m[Caution: Firewall is active, however is missing a chain. Address this issue immediately.]\033[0m")
                 else:
-                    os.system("echo "+"Firewall Status: "+"\033[031m[INACTIVE]\033[0m")
+                    print("Firewall Status: \033[31;1m[INACTIVE]\033[0m")
                 if(blacklistPres):
-                    os.system("echo "+"Blacklist Status: "+"\033[032m[GREEN]\033[0m")
+                    print("Blacklist Status: \033[32;1m[GREEN]\033[0m")
                     if(not blacklistInteg):
-                        os.system("echo "+"\033[1;33m[Caution: Blacklist is active, however is missing a chain. Address this issue immediately.]\033[0m")
+                        print("\033[33;1m[Caution: Blacklist is active, however is missing a chain. Address this issue immediately.]\033[0m")
                 else:
-                    os.system("echo "+"Blacklist Status: "+"\033[031m[INACTIVE]\033[0m")
+                    print("Blacklist Status: "+"\033[31;1m[INACTIVE]\033[0m")
                 if(otherTablePres):
-                    os.system("echo "+"\033[133m[Caution: Other tables detected present in nfTables. If this is unexpected, please investigate the issue.]\033[0m")
+                    print("\033[33;1m[Caution: Other tables detected present in nfTables. If this is unexpected, please investigate the issue.]\033[0m")
                 if(firewallPres and firewallInteg and blacklistPres and blacklistInteg):
                     print("\n")
                     ports = [[]]

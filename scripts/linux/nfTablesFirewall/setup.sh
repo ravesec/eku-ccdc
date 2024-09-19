a9ac016ed4c073778bc89d8e144e819a71ce087c
Sam Burton <sam.burton271@gmail.com>
2024-09-19 11:09:12 -0400

    Made the setup script directory independent

--- scripts/linux/nfTablesFirewall/setup.sh
+++ scripts/linux/nfTablesFirewall/setup.sh
@@ -2,9 +2,10 @@
 apt-get update && apt-get install -y nftables 
 apt-get update && apt-get install -y python3 
 yum install -y nftables 
-yum install -y python3 
-mv /etc/eku-ccdc/scripts/linux/nfTablesFirewall/firewall.py /bin/firewall
+yum install -y python3
+repo_root=$(git rev-parse --show-toplevel) 
+mv $repo_root/scripts/linux/nfTablesFirewall/firewall.py /bin/firewall
 chmod +x /bin/firewall
 echo "Defaults env_keep += \"SSH_CONNECTION SSH_CLIENT SSH_TTY\"" >> /etc/sudoers
-python3 /etc/eku-ccdc/scripts/linux/nfTablesFirewall/setup.py
+python3 $repo_root/scripts/linux/nfTablesFirewall/setup.py
 rm $0

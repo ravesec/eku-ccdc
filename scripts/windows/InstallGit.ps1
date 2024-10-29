127990783d9de93ec0c4814e90e26f6a5f60be5b
c-u-r-s-e <loganjack627@gmail.com>
2024-10-29 11:46:06 -0400

    Fixing web request

--- scripts/windows/InstallGit.ps1
+++ scripts/windows/InstallGit.ps1
@@ -1,7 +1,7 @@
 $webClient = (New-Object System.Net.WebClient)
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
-$webClient.DownloadFile("https://github.com/git-for-windows/git/releases/download/v2.47.0.windows.2/Git-2.47.0.2-64-bit.exe","$env:USERPROFILE\Downloads")
+$webClient.DownloadFile("https://github.com/git-for-windows/git/releases/download/v2.47.0.windows.2/Git-2.47.0.2-64-bit.exe","$env:USERPROFILE\Downloads\Git-2.47.0.2-64-bit.exe")
 Set-Location "$env:USERPROFILE\Downloads"
 Start-Process Git-2.47.0.2-64-bit.exe /VERYSILENT /NORESTART -Wait
 Remove-Item "Git-2.47.0.2-64-bit.exe"

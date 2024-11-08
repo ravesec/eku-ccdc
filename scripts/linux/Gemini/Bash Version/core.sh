b7e70bac4c850c2514fa6af2edda425b1d447d06
Sam Burton <sam.burton271@gmail.com>
2024-11-08 15:33:49 -0500

    Bug fixes

--- scripts/linux/Gemini/Bash Version/core.sh
+++ scripts/linux/Gemini/Bash Version/core.sh
@@ -23,10 +23,10 @@ while true; do
 getFileCont "/etc/passwd" passwdConts
 for line in "${passwdConts[@]}"; do
 	IFS=":" read -ra userInfo <<< "$line"
-	username=$userInfo[0]
-    declare -i uid=$userInfo[2]
-    declare -i gid=$userInfo[3]
-	if [[ [[ $uid -gt 999 ]] || [[ $gid -gt 999 ]] && ! userInWhitelist $username ]]; then
+	username=${userInfo[0]}
+    declare -i uid=${userInfo[2]}
+    declare -i gid=${userInfo[3]}
+	if [[ $uid -gt 999 || $gid -gt 999 ]] && [[ ! userInWhitelist $username ]]; then
 		userdel -f $username
 		current_time=$(date +"%H:%M:%S")
 		log="[ $current_time ] - An unknown user with UID/GID above 999 was found and removed: $username"

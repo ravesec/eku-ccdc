1c2e50f75ecfee7d04cb7390983c3b6c8925f41b
Sam Burton <sam.burton271@gmail.com>
2024-11-12 20:14:51 -0500

    Updated installer.

--- scripts/linux/Gemini/Bash Version/install.sh
+++ scripts/linux/Gemini/Bash Version/install.sh
@@ -5,5 +5,7 @@ mv "$repo_root/scripts/linux/Gemini/Bash Version/core.sh" /etc/gemini/core
 chmod +x /etc/gemini/core
 mv "$repo_root/scripts/linux/Gemini/Bash Version/gemini.service" /etc/systemd/system/gemini.service
 systemctl daemon-reload
-systemctl enable gemini
-systemctl start gemini
+echo "Gemini installed, but has not started. Please edit the file located at /etc/gemini/core.sh and make changes to the settings as needed before starting."
+echo "Once settings have been changed, start Gemini by running these two commands:"
+echo "systemctl enable gemini.service"
+echo "systemctl start gemini.service"

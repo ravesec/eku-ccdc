436dad6c34baaf2c1f83d1a25fc9aa2fae64d301
Sam Burton <sam.burton271@gmail.com>
2024-11-12 19:31:25 -0500

    Removed '/bin/nc' from reverse shell flags due to necessity of ncat for gemini comms

--- scripts/linux/Gemini/Bash Version/core.sh
+++ scripts/linux/Gemini/Bash Version/core.sh
@@ -6,6 +6,7 @@ numbers=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9")
 whitelistUsers=("root" "sysadmin" "sshd" "sync" "_apt" "nobody")
 suspiciousFileNames=("shell.php" "template.php")
 suspiciousServices=("minecraft" "discord" "snapchat" "systemb")
+revShellFlags=("import pty" "pty.spawn")
 getFileContAsArray() #usage: "getFileCont {file name} {array variable name}"
 {
 	local fileName="$1"
--- scripts/linux/Gemini/localGemini.py
+++ scripts/linux/Gemini/localGemini.py
@@ -8,7 +8,7 @@ upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 whiteListUsers = ["root", "sysadmin", "sshd", "sync", "_apt", "nobody"]
-revShellFlags = ["/bin/nc", "import pty", "pty.spawn"]
+revShellFlags = ["import pty", "pty.spawn"]
 suspiciousServices = ["system(x)", "discord.exe", "snapchat.exe", "minecraft.exe"]
 serviceOverride = ["systemd"]
 suspiciousFileNames = ["shell.php", "template.php"]

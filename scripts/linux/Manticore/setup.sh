e99e50fa085d1f394071290f734e26b8c85b25a1
Sam Burton <sam.burton271@gmail.com>
2024-11-14 17:41:54 -0500

    Edited setup for competition environment

--- scripts/linux/Manticore/setup.sh
+++ scripts/linux/Manticore/setup.sh
@@ -139,12 +139,12 @@ systemctl enable manticore
 systemctl start manticore
 rm /tmp/manticoreSetup
 EOFA
-echo "Setting up E-Comm listener..."
-python3 /etc/manticore/netListenerSetup.py "172.20.241.30"
-echo "Setting up Fedora listener..."
-python3 /etc/manticore/netListenerSetup.py "172.20.241.40"
-echo "Setting up Debian listener..."
-python3 /etc/manticore/netListenerSetup.py "172.20.240.20"
+#echo "Setting up E-Comm listener..."
+#python3 /etc/manticore/netListenerSetup.py "172.20.241.30"
+#echo "Setting up Fedora listener..."
+#python3 /etc/manticore/netListenerSetup.py "172.20.241.40"
+#echo "Setting up Debian listener..."
+#python3 /etc/manticore/netListenerSetup.py "172.20.240.20"
 #echo "Setting up Ubuntu listener..."
 echo "Installing Gemini Server..."
 bash $repo_root/scripts/linux/Gemini/install.sh "-s"

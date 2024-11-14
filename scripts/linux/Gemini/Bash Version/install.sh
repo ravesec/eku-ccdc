92c17270c2794eb451d5586b50f9e883be02ce24
Sam Burton <sam.burton271@gmail.com>
2024-11-14 12:15:01 -0500

    Bug fixes

--- scripts/linux/Gemini/Bash Version/install.sh
+++ scripts/linux/Gemini/Bash Version/install.sh
@@ -6,23 +6,23 @@ mv "$repo_root/scripts/linux/Gemini/Bash Version/core.sh" /etc/gemini/core
 chmod +x /etc/gemini/core
 mv "$repo_root/scripts/linux/Gemini/Bash Version/gemini.service" /etc/systemd/system/gemini.service
 systemctl daemon-reload
-declare -i x=3
-machineList=("centos" "ecom" "fedora" "debian" "ubuntu")
-while [[ x -eq 3 ]]; do
-	echo "Enter your machine name(centos/fedora/debian/ubuntu): "
-	read machine
-	for item in "${machineList[@]}"); do
-		if [[ machine == item ]]; then
-			$x=4
+if [[ ! -z "$1" ]] && [[ ! "$1" == "-s" ]]; then
+	declare -i x=3
+	machineList=("centos" "ecom" "fedora" "debian" "ubuntu")
+	while [[ x -eq 3 ]]; do
+		echo "Enter your machine name(centos/fedora/debian/ubuntu): "
+		read machine
+		for item in "${machineList[@]}"; do
+			if [[ machine == item ]]; then
+				$x=4
+			fi
+		done
+		if [[ x -eq 3 ]]; then
+			echo "Invalid entry."
 		fi
 	done
-	if [[ x -eq 3 ]]; then
-		echo "Invalid entry."
-	fi
-done
-touch /etc/gemini/machine.name
-echo $machine >> machine.name
-if [[ ! -z "$1" ]] && [[ ! "$1" == "-s" ]]; then
+	touch /etc/gemini/machine.name
+	echo $machine >> machine.name
 	echo "Gemini installed, but has not started. Please edit the file located at /etc/gemini/core.sh and make changes to the settings as needed before starting."
 	echo "Once settings have been changed, start Gemini by running these two commands:"
 	echo "systemctl enable gemini.service"

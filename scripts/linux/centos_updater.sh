ff2034e705c7eb493f51efe3e9b2a2e29666c88c
Raven Dean <104028164+ravesec@users.noreply.github.com>
2025-02-06 18:49:00 -0500

    Create centos_updater.sh

---
+++ scripts/linux/centos_updater.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ] # Superuser requirement.
+then error "This script must be ran as root!"
+    exit 1
+fi
+
+# Update repos
+mkdir /etc/yum.repos.d/old
+mv /etc/yum.repos.d/CentOS*.repo /etc/yum.repos.d/old/
+mv /etc/yum.repos.d/epel*.repo /etc/yum.repos.d/old/
+
+curl -o /etc/yum.repos.d/CentOS.repo https://raw.githubusercontent.com/ravesec/eku-ccdc/refs/heads/main/config_files/CentOS.repo
+curl -o /etc/yum.repos.d/epel.repo https://raw.githubusercontent.com/ravesec/eku-ccdc/refs/heads/main/config_files/epel.repo
+
+yum clean all
+yum update
+
+# Get rid of broken repos
+rm /etc/yum.repos.d/CentOS-*
+echo "Update complete. If you run 'yum update' again, make sure to delete the broken repos that it adds to /etc/yum.repos.d/"
+
+exit 0 # Script ended successfully
